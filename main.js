0a67352ad41417eced9cdb27d381f89a5ad70ad2
Esteban Echegoyen <echegoyenesteban@gmail.com>
2023-10-20 18:04:35 -0300

    Se quita función de evento, se agrega ciclo do while, con validación de números positivos y válidos, con alerta ccorrespondiente, y se agrega bucle para que puedan realizar otros cálculos

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    const principal = parseFloat(prompt("Ingrese monto que desea utilizar para el plazo fijo:"));
-    const interesAnual = parseFloat(prompt("Ingrese la Tasa de Interés Anual (TNA) (%): Ej: Plazo fijo tradicional 133%")) / 100;
-    const duracionMeses = parseInt(prompt("Ingresa el Plazo en meses:"));
+function calcularPlazoFijo() {
+    let continuar = true;
 
-        if (isNaN(principal) || isNaN(interesAnual) || isNaN(duracionMeses)) {
-            console.log("Por favor, ingresa valores válidos.");
-        } else {
-            const interesGanado = principal * (interesAnual / 12) * duracionMeses;
-            const montoTotal = principal + interesGanado;
+    while (continuar) {
+        let principal, interesAnual, duracionMeses;
 
-            console.log("Resultados:");
-            console.log(`Intereses ganados: $${interesGanado.toFixed(2)}`);
-            console.log(`Monto final con intereses ganados: $${montoTotal.toFixed(2)}`);
-        }
-    });
+        do {
+            principal = parseFloat(prompt("Ingrese el monto que desea utilizar para el plazo fijo:"));
+            if (isNaN(principal) || principal < 0) {
+                alert("Por favor, ingrese un valor válido y positivo para el monto.");
+            }
+        } while (isNaN(principal) || principal < 0);
+
+        do {
+            interesAnual = parseFloat(prompt("Ingrese la Tasa de Interés Anual (TNA) (%): Ej: Plazo fijo tradicional 13.3%")) / 100;
+            if (isNaN(interesAnual) || interesAnual < 0) {
+                alert("Por favor, ingrese un valor válido y positivo para la tasa de interés anual.");
+            }
+        } while (isNaN(interesAnual) || interesAnual < 0);
+
+        do {
+            duracionMeses = parseInt(prompt("Ingresa el Plazo en meses:"));
+            if (isNaN(duracionMeses) || duracionMeses < 0) {
+                alert("Por favor, ingrese un valor válido y positivo para la duración en meses.");
+            }
+        } while (isNaN(duracionMeses) || duracionMeses < 0);
+
+    
+        const interesGanado = principal * (interesAnual / 12) * duracionMeses;
+        const montoTotal = principal + interesGanado;
+
+        console.log("Resultados:");
+        console.log(`Intereses ganados: $${interesGanado.toFixed(2)}`);
+        console.log(`Monto final con intereses ganados: $${montoTotal.toFixed(2)}`);
+        
+        
+        const decision = prompt("Desea realizar otro cálculo de plazo fijo? (Si/No)").toLowerCase();
+        continuar = decision === "si" || decision === "sí";
+    }
+}
+
+calcularPlazoFijo();
